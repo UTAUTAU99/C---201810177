b8b02a609f6c23a9a4f6af248a361f287cb10924
UTAUTAU <twjung9923@gmail.com>
2023-09-15 15:50:41 +0900

    9.15

--- hw1.c
+++ hw1.c
@@ -8,8 +8,8 @@ int main(void)
     int num3 = num1 & num2;
     int num4 = num1 | num2;
     int num5 = num1 ^ num2;
-    printf(" %d \n", num3);
-    printf(" %d \n", num4);
-    printf(" %d \n", num5);
+    printf("%d & %d = %d \n", num1, num2, num3);
+    printf("%d | %d = %d \n", num1, num2, num4);
+    printf("%d ^ %d = %d \n", num1, num2, num5);
     return 0;
 }
