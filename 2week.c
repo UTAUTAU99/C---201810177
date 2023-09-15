b675d27fb9abc213095029f70ea788d7cb690083
UTAUTAU <twjung9923@gmail.com>
2023-09-15 12:17:12 +0900

    ㅇㄹ

--- 2week.c
+++ 2week.c
@@ -1,11 +1,14 @@
+#include <stdio.h>
 int main(void)
 {
-    int num1 = +2;
-    int num2 = -4;
-
-    num1= -num1;
-    printf("num1: %d \n", num1);
-    num2 = -num2;
-    printf("num2: %d \n", num2);
+    int num1=10;
+    int num2=12;
+    int result1, result2, result3;
+    result1=(num1==num2);
+    result2=(num1<=num2);
+    result3=(num1>num2);
+    printf("result1: %d \n", result1);
+    printf("result2: %d \n", result2);
+    printf("result3: %d \n", result3);
     return 0;
 }
--- 2주차.c
+++ 2주차.c
@@ -1,1 +1,1 @@
-<preference name="deployment-target" value="11.0" />
+#include <stdio.h>
