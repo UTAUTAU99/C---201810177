8bf753883855c8d5e2ec9888ae8a22af5b2beb60
UTAUTAU <twjung9923@gmail.com>
2023-10-06 13:25:50 +0900

    10.06

--- practice.c
+++ practice.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 int main(void)
 {
-    int num, space, star;
-    for(num=0; num<5; num++)
+    int n;
+    printf("n을 입력 : ");
+    scanf("%d", &n);
+    switch(n)
     {
-        for(space=4; space>0; space--)
-            printf(" ");
-        {
-            for(star=1; star<11; space++2)
-                printf("*");
-        }
+    case(0<= &n <10):
+        printf("0이상 10미만");
+        break;
+    case(10<= &n <20):
+        printf("10이상 20미만");
+        break;
+    case(20<= &n <30):
+        printf("20이상 30미만");
+        break;
+    case(30<= &n) :
+        printf("30이상");
+        break;
     }
     return 0;
 }
