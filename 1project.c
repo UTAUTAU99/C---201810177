4e1cd1a06523180be7e567cead75e19690aa67c7
UTAUTAU <twjung9923@gmail.com>
2023-10-15 16:39:23 +0900

    10.15

--- 1project.c
+++ 1project.c
@@ -1,88 +1,102 @@
 #include <stdio.h>
 
-int main(void)
-{
-    char payment, membership;
-    int price1;
-    printf("=== 네이버 결제 계산 프로그램 ===\n주문액을 입력하세요: ");
-    scanf("%d", &price1);
-    printf("=== 결제 수단 목록 === \na)NPay머니 네이버통장 충전결제 b)NPay머니 일반 충전결제 c)계좌 간편결제 \nd)네이버 현대카드 간편결제e)청구할인 삼성카드결제 f)일반 카드결제\n");
+int main() {
+    int orderAmount;
+    char paymentMethod;
+    char naverMembership;
 
-    while (1)
-    {
+    printf("=== 네이버페이 결제 계산 프로그램 ===\n");
+    printf("주문액을 입력하세요: ");
+    scanf("%d", &orderAmount);
+
+    printf("=== 결제 수단 목록 ===\n");
+    printf("a) NPay머니 네이버통장 충전결제 ");
+    printf("b) NPay머니 일반 충전결제 ");
+    printf("c) 계좌 간편결제\n");
+    printf("d) 네이버 현대카드 간편결제 ");
+    printf("e) 청구할인 삼성카드결제 ");
+    printf("f) 일반 카드결제\n");
+    printf("결제 수단을 입력하세요: ");
+    scanf(" %c", &paymentMethod);
+
+    while (paymentMethod < 'a' || paymentMethod > 'f') {
+        printf("잘못 입력하셨습니다. 재입력 바랍니다.\n");
         printf("결제 수단을 입력하세요: ");
-        scanf(" %c", &payment);
-        if ('a'<= payment && payment <='f')
-        {
+        scanf(" %c", &paymentMethod);
+    }
+
+    
+    printf("네이버 멤버십에 가입되어 있나요 (y/n)? ");
+    scanf(" %c", &naverMembership);
+
+    int basicPoints = orderAmount * 0.01;
+    int naverHyundaiCardPoints;
+    if(orderAmount>200000)
+        naverHyundaiCardPoints=10000;
+    else naverHyundaiCardPoints = orderAmount * 0.05;
+
+    int SamSungCard;
+    if(orderAmount<50000)
+        SamSungCard=0;
+        if(orderAmount<100000)
+            SamSungCard = orderAmount*0.05;
+        else
+            SamSungCard = 5000;
+
+    printf("=== 결제 및 적립 내역 ===\n");
+    printf("주문금액 %d원\n", orderAmount);
+    printf("기본 적립 %d원\n", basicPoints);
+
+    switch (paymentMethod) {
+        case 'a':
+            printf("네이버페이 머니 결제적립 %.0f원\n", basicPoints * 1.5);
+            printf("네이버통장 추가적립 %.0f원\n", basicPoints * 0.5);
             break;
-        }
-        else {
-            printf("잘못 입력하셨습니다. 재입력 바랍니다\n");
-            continue;
-        }
+        case 'b':
+            printf("네이버페이 머니 결제적립 %.0f원\n", basicPoints * 1.5);
+            break;
+        case 'd':
+            if (naverHyundaiCardPoints > 10000) {
+                naverHyundaiCardPoints = 10000;
+            }
+            printf("네이버 현대카드 추가적립 %d원\n", naverHyundaiCardPoints);
+            break;
+        case 'e':
+            if (orderAmount >= 50000) {
+                printf("청구할인 %d원\n", SamSungCard);
+            }
+            break;
+        default:
+            break;
     }
-    int price2=price1*0.01, price3=price1*0.015, price4=price1*0.005, price5=price1*0.05, price6=price1*0.05, price7=price1*0.04;
-    //1.주문금액, 2.기본적립 3.Npay머니 결제적립 4.네이버통장 추가결제 5.네이버현대카드 간편결제 6.청구할인 삼성카드 7.네이버멤버십
-    printf("네이버 멤버십에 가입되어 있나요(y/n)? ");
-    scanf("%s", &membership);
-    if (membership == 'Y' || membership == 'y')
-    {
-        switch(payment)
-        {
-            case 'a' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-                       &price1, &price2, &price3, &price4, &price7, &price7);
-                break;
-            case 'b' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-                       &price1, &price2, &price3, &price7);
-                break;
-            case 'c' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립:%d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-                       &price1, &price2, &price7);
-                break;
-            case 'd' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버 현대카드 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-                       &price1, &price2, &price5, &price7);
-                break;
-            case 'e' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n청구할인 %d\n기본 적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-                       &price1, &price6, &price2, &price7);
-                break;
-            case 'f' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-                       &price1, &price2, &price7);
-                break;
-        }
+
+    int naverMembershipPoints;
+    if (naverMembership == 'y' || naverMembership == 'Y') {
+        if(orderAmount>1400000)
+            naverMembershipPoints=20000;
+        else if (orderAmount < 200000) {
+                 naverMembershipPoints = orderAmount * 0.04;
+            } else {
+                naverMembershipPoints = 8000 + (orderAmount - 200000) * 0.01;
+            }
+            printf("네이버 멤버십 추가적립 %d원\n", naverMembershipPoints);
     }
-    else{
-            switch(payment)
-        {
-            case 'a' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n총 적립액은 %d원입니다.\n",
-                &price1, &price2, &price3, &price4);
-                break;
-            case 'b' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버통장 추가적립: %d\n총 적립액은 %d원입니다.\n",
-                &price1, &price2, &price3);
-                break;
-            case 'c' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립:%d\n총 적립액은 %d원입니다.\n",
-                &price1, &price2);
-                break;
-            case 'd' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버 현대카드 추가적립: %d\n총 적립액은 %d원입니다.\n",
-                &price1, &price2, &price5);
-                break;
-            case 'e' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n청구할인 %d\n기본 적립: %d\n총 적립액은 %d원입니다.\n",
-                &price1, &price6, &price2);
-                break;
-            case 'f' :
-                printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n총 적립액은 %d원입니다.\n",
-                &price1, &price2);
-                break;
-        }
+    
+    int totalPoints = basicPoints;
+    if (paymentMethod == 'a' || paymentMethod == 'b') {
+        totalPoints += basicPoints * 2;
     }
+    if (paymentMethod == 'd' && naverHyundaiCardPoints > 0) {
+        totalPoints += naverHyundaiCardPoints;
+    }
+    if (orderAmount >= 50000 && paymentMethod == 'e') {
+        totalPoints += 5000;
+    }
+    if (naverMembershipPoints > 0) {
+        totalPoints += naverMembershipPoints;
+    }
+
+    printf("총 적립액은 %d원입니다.\n", totalPoints);
+
     return 0;
 }
--- project 1.c
+++ project 1.c
@@ -1,72 +1,89 @@
 #include <stdio.h>
 
-int main(void)
-{
-    char payment, membership;
-    int price1, price2, price3, price4, price5, price6, price7, pirce8;
-    printf("=== 네이버 결제 계산 프로그램 ===\n주문액을 입력하세요: ");
-    scanf("%d", &price1);
-    printf("=== 결제 수단 목록 === \na)NPay머니 네이버통장 충전결제 bNPay머니 일반 충전결제 c)계좌 간편결제 \nd)네이버 현대카드 간편결제e)청구할인 삼성카드결제 f)일반 카드결제\n");
-    
-    while (1)
-    {
+int main() {
+    int orderAmount;
+    char paymentMethod;
+    char naverMembership;
+
+    printf("== 네이버페이 결제 계산 프로그램 ==\n");
+    printf("주문액을 입력하세요: ");
+    scanf("%d", &orderAmount);
+
+    printf("== 결제 수단 목록 ==\n");
+    printf("a) NPay머니 네이버통장 충전결제\n");
+    printf("b) NPay머니 일반 충전결제\n");
+    printf("c) 계좌 간편결제\n");
+    printf("d) 네이버 현대카드 간편결제\n");
+    printf("e) 청구할인 삼성카드결제\n");
+    printf("f) 일반 카드결제\n");
+    printf("결제 수단을 입력하세요: ");
+    scanf(" %c", &paymentMethod);
+
+    while (paymentMethod < 'a' || paymentMethod > 'f') {
+        printf("잘못 입력하셨습니다. 재입력 바랍니다.\n");
         printf("결제 수단을 입력하세요: ");
-        scanf("%s", &payment);
-        if ('a'<= payment <='f')
-        {
+        scanf(" %c", &paymentMethod);
+    }
+
+    
+    printf("네이버 멤버십에 가입되어 있나요 (y/n)? ");
+    scanf(" %c", &naverMembership);
+
+    int basicPoints = orderAmount * 0.01;
+
+    printf("== 결제 및 적립 내역 ==\n");
+    printf("주문금액 %d원\n", orderAmount);
+    printf("기본 적립 %d원\n", basicPoints);
+
+    switch (paymentMethod) {
+        case 'a':
+            printf("네이버페이 머니 결제적립 %d원\n", basicPoints * 1.5);
+            printf("네이버통장 추가적립 %d원\n", basicPoints * 0.5);
             break;
-        }
-        else {
-            printf("잘못 입력하셨습니다. 재입력 바랍니다\n");
-            continue;
-        }
+        case 'b':
+            printf("네이버페이 머니 결제적립 %d원\n", basicPoints * 1.5);
+            break;
+        case 'd':
+            naverHyundaiCardPoints = (int)(basicPoints * 0.05);
+            if (naverHyundaiCardPoints > 10000) {
+                naverHyundaiCardPoints = 10000;
+            }
+            printf("네이버 현대카드 추가적립 %d원\n", naverHyundaiCardPoints);
+            break;
+        case 'e':
+            if (orderAmount >= 50000) {
+                printf("청구할인 5000원\n");
+            }
+            break;
+        default:
+            break;
     }
-    printf("네이버 멤버십에 가입되어 있나요(y/n)? ");
-    scanf("%s", &membership);
-    if (membership == 'Y' || membership == 'y') {
-        switch(payment)
-        {
-            case 'a' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-            case 'b' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-            case 'c' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-            case 'd' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-            case 'e' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-            case 'f' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
+
+    int naverMembershipPoints;
+    if (naverMembership == 'y' || naverMembership == 'Y') {
+        if (orderAmount < 200000) {
+            naverMembershipPoints = orderAmount * 0.04;
+        } else {
+            naverMembershipPoints = 20000 + (orderAmount - 200000) * 0.01;
         }
-    else
-            switch(payment)
-        {
-            case 'a' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-            case 'b' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-            case 'c' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-            case 'd' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-            case 'e' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-            case 'f' :
-            printf("=== 결제 및 적립내역 ===\n주문금액 : %d\n기본 적립: %d\n네이버페이 머니 결제적립: %d\n네이버통장 추가적립: %d\n네이버 멤버십 추가적립: %d\n총 적립액은 %d원입니다.\n",
-            &price1);
-        }
+        printf("네이버 멤버십 추가적립 %d원\n", naverMembershipPoints);
     }
+
+    int totalPoints = basicPoints;
+    if (paymentMethod == 'a' || paymentMethod == 'b') {
+        totalPoints += basicPoints * 2;
+    }
+    if (paymentMethod == 'd' && naverHyundaiCardPoints > 0) {
+        totalPoints += naverHyundaiCardPoints;
+    }
+    if (orderAmount >= 50000 && paymentMethod == 'e') {
+        totalPoints += 5000;
+    }
+    if (naverMembershipPoints > 0) {
+        totalPoints += naverMembershipPoints;
+    }
+
+    printf("총 적립액은 %d원입니다.\n", totalPoints);
+
     return 0;
 }
