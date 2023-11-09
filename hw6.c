b711a0e05050b60b23948985beb56eebb4e58c21
UTAUTAU <twjung9923@gmail.com>
2023-11-09 10:19:34 +0900

    11.9

---
+++ hw6.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void odd(int* ptr)
+{
+	int i;
+	printf("Odd numbers:");
+	for (i = 0; i < 5; ++i) {
+		if (ptr[i] % 2 != 0)
+			printf(" %d,", ptr[i]);
+	}
+	printf("\b \n");
+}
+
+void even(int* ptr)
+{
+	int i;
+	printf("Even numbers:");
+	for (i = 0; i < 5; ++i) {
+		if (ptr[i] % 2 == 0)
+			printf(" %d,", ptr[i]);
+	}
+	printf("\b \n");
+}
+
+int main(void)
+{
+	int i,a[10];
+	printf("Please input five integers: ");
+	for (i = 0; i < 5; ++i) {
+		scanf("%d", &a[i]);
+	}
+	odd(a);
+	even(a);
+
+	return 0;
+}
--- practice.c
+++ practice.c
@@ -1,2 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(void)
+{
+    int i;
+    int arr[5];
+
+    printf("Please input five numbers : ");
+    scanf("%c", &arr);
+    printf("%c", &arr);
+}
