73fd58bc499efab67da7666b2d566e80fc07d1f9
UTAUTAU <twjung9923@gmail.com>
2023-12-01 15:21:40 +0900

    12.1

--- practice.c
+++ practice.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+
+typedef struct point
+{
+    int xpos;
+    int ypos;
+} Point;
+
+typedef struct circle
+{
+    Point cen;
+    double rad;
+} Circle;
 
 int main(void)
 {
-    char name1[20]="0", name2[20]="0";
-    int age1, age2;
-
-    printf("첫 번째 사람 정보 입력: ");
-    scanf_s("%s", name1, sizeof(name1));
-    scanf_s("%d", &age1);
-    
-    printf("첫 번째 사람 정보 입력: ");
-    scanf_s("%s", name1, sizeof(name1));
-    scanf_s("%d", &age1);
+    return 0;
 }
